396d28166fc26d4cba85842ee640c3e5706ab7dc
gshen1-SGZ <gshen1@drew.edu>
2026-03-23 13:03:48 -0400

    Add personal settings page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import RegisterView, CustomLoginView
+from .views import RegisterView, CustomLoginView, ProfileUpdateView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('settings/', ProfileUpdateView.as_view(), name='account_settings'),
 ]
